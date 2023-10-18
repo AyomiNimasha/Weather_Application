b877a04b8a73671f7199212dbbfc3ca9e653ff5e
AyomiNimasha <ayominimasha@gmail.com>
2023-10-18 17:02:57 +0530

    commit 01

---
+++ src/components/EndPoint.js
@@ -0,0 +1,54 @@
+ import axios from "axios";
+
+// const FetchData = async () => {
+//   try {
+//     const response = await axios.get(
+//       "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=39b0f2796cde29f0894780182f90ade7"
+//     );
+//     return response.data;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+// export default FetchData;
+
+// export const FetchThreeDaysData=async(cityName)=>{
+
+// }
+import React from "react";
+
+export const SearchCurrenthData = async (cityName) => {
+    try {
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=39b0f2796cde29f0894780182f90ade7`)
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+
+};
+
+//for threee days(four days link)
+export const ThreeDaysForecast = async (cityName) => {
+  try {
+  const response = await axios.get(`https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid=39b0f2796cde29f0894780182f90ade7`)
+  console.log(response);
+  return response.data;
+} catch (error) {
+  throw error;
+}
+
+};
+
+//for week forecast
+export const NextWeekForecast = async (cityName) => {
+  try {
+  const response = await axios.get(`api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={7}&appid=39b0f2796cde29f0894780182f90ade7`)
+  console.log(response);
+  return response.data;
+} catch (error) {
+  throw error;
+}
+
+};
+
---
+++ src/components/FetchData.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import { SearchCurrenthData,ThreeDaysForecast,NextWeekForecast } from './EndPoint';
+
+function FetchData(){
+    const [cityName,setcityName]=useState('colombo');
+    const [currentWeather,setCurrentWether]=useState({});
+    const [threeDaysForecast,setThreeDaysForecast]=useState({});
+    const [nextWeekForecast,setNextWeekForecast]=useState({});
+
+    useEffect(()=>{
+        SearchCurrenthData(cityName)
+        .then((data)=>setCurrentWether(data))
+        .catch((error)=>console.log('Error',error))
+
+        ThreeDaysForecast(cityName)
+        .then((data)=>setThreeDaysForecast(data))
+        .catch((error)=>console.log('Error',error))
+
+        NextWeekForecast(cityName)
+        .then((data)=>setNextWeekForecast(data))
+        .catch((error)=>console.log('Error',error))
+
+    },[cityName])
+  return (
+    <div>FetchData</div>
+  )
+}
+export default FetchData;
